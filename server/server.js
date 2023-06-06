0a5d49e4df4445e3cc46a52e9610d24810112c68
palmtopj <palmtopj@uw.edu>
2023-06-06 02:15:07 -0700

    Cant get Server.js mssg Hello

--- server/server.js
+++ server/server.js
@@ -3,7 +3,12 @@ const express = require('express')
 // creates express
 const app = express()
 
+// routes
+app.get('/', (req, res) => {
+    res.json({mssg: 'Hello World!'})
+})
+
 app.listen(3001, () => {
-    console.log('Server listening on port 3001!')
+    console.log('Server is listening on port 3001!')
 })
 
