8c0754df9c7c4510c531576947244eb7214aad61
palmtopj <palmtopj@uw.edu>
2023-06-06 01:54:12 -0700

    package.json express nodemon dependencies

--- server/server.js
+++ server/server.js
@@ -4,6 +4,6 @@ const express = require('express')
 const app = express()
 
 app.listen(3001, () => {
-    console.log('Server listening on port 3001')
+    console.log('Server listening on port 3001!')
 })
 
